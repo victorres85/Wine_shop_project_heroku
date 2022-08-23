a43fbd8e37e34e8f4c38c5ddd2eff198fd4980f4
Victor <victorres.almeida@gmail.com>
2022-08-23 21:49:32 +0100

    deploying on heroku

--- wine_shop/celery.py
+++ wine_shop/celery.py
@@ -11,6 +11,6 @@ app = Celery('wine_shop')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
-#@app.task(bind=True)
-#def debug_task(self):
-#    print('Request: {0!r}'.format(self.request))
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
