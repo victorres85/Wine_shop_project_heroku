a4503d1be419883a645affa98f063be86c2eab01
Victor <victorres.almeida@gmail.com>
2022-08-28 17:49:02 +0100

    heroku

--- wine_shop/celery.py
+++ wine_shop/celery.py
@@ -9,13 +9,11 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wine_shop.settings')
 
-app = Celery('wine_shop')
+app = Celery('wine_shop', broker='redis://:pcbbab5e12031f5fee7b14e4eae72e04d94cc5d4b72e880c9e6171313bc6b46bc@ec2-54-194-139-149.eu-west-1.compute.amazonaws.com:25540')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-app.conf.update(BROKER_URL=os.environ('REDIS_URL'),
-                CELERY_RESULT_BACKEND=os.environ('REDIS_URL'))
 
 
 @app.task(bind=True)
